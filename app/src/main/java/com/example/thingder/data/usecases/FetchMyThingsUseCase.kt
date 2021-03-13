437d5876f6ac3f4235961c676fb8e5e9eb2e3b81
Aleksandr Evdokimov <alexandr8@gmail.com>
2021-03-13 19:39:15 +0300

    FetchMyThingsUseCase: add mapping, hardcode thing id

--- app/src/main/java/com/example/thingder/data/usecases/FetchMyThingsUseCase.kt
+++ app/src/main/java/com/example/thingder/data/usecases/FetchMyThingsUseCase.kt
@@ -1,9 +1,11 @@
 package com.example.thingder.data.usecases
 
+import android.os.Parcel
 import com.example.thingder.domain.entities.Thing
 import com.example.thingder.domain.usecases.IFetchMyThingsUseCase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
 import kotlinx.coroutines.flow.*
 
 class FetchMyThingsUseCase(
@@ -19,18 +21,19 @@ class FetchMyThingsUseCase(
     private val stateSharedFlow = MutableStateFlow<List<Thing>>(emptyList())
 
     override fun fetch(): Flow<List<Thing>> {
-
         document.addSnapshotListener { value, error ->
             val list = mutableListOf<Thing>()
-            value?.forEach {
-                //TODO: return id
-                //val id = it["id"] as Int
-                val title = it["title"].toString()
-                list.add(Thing(3, title))
-            }
+            value?.map { it -> it.toDomain() }
             stateSharedFlow.value = list
         }
         return stateSharedFlow
+    }
+
+    private fun QueryDocumentSnapshot.toDomain(): Thing {
+        //TODO: return id
+        //val id = this["id"] as Int
+        val title = this["title"].toString()
+        return Thing(3, title)
     }
 
 }
