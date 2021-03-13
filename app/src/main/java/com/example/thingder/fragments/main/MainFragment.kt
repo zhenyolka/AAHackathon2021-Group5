adc056165edc4ef025c9c1c08626c36703723e7d
MikhailN45 <shelove6391@gmail.com>
2021-03-14 00:34:48 +0500

    cardStackSize changed to 2 add gradient

--- app/src/main/java/com/example/thingder/fragments/main/MainFragment.kt
+++ app/src/main/java/com/example/thingder/fragments/main/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.asynctaskcoffee.cardstack.CardListener
+import com.asynctaskcoffee.cardstack.px
 import com.example.thingder.R
 import com.example.thingder.data.usecases.tinder.DislikeThingUseCase
 import com.example.thingder.data.usecases.tinder.FetchNearbyThingUseCase
@@ -32,6 +33,11 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         binding.cardContainer.setOnCardActionListener(object : CardListener {
             override fun onItemShow(position: Int, model: Any) {
                 /*Current card model on screen*/
+                binding.cardContainer.apply {
+                    this.maxStackSize = 2
+                    this.marginTop = 20.px
+                }
+
             }
 
             override fun onLeftSwipe(position: Int, model: Any) {
@@ -49,7 +55,6 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             override fun onSwipeCompleted() {
                 /*Called when there is no cards*/
             }
-
         })
     }
 }
