74e26acbc259e03db626e51aa410244708290a08
Aleksandr Evdokimov <alexandr8@gmail.com>
2021-03-13 23:26:43 +0300

    Add logout option

--- app/src/main/java/com/example/thingder/MainActivity.kt
+++ app/src/main/java/com/example/thingder/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.thingder
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.thingder.activities.login.LoginActivity
 import com.example.thingder.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,5 +39,45 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.logout -> {
+                showLogoutDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showLogoutDialog() {
+        val builder = AlertDialog.Builder(this)
+            .setTitle(getString(R.string.logout_alert_dialog_title))
+            .setMessage(getString(R.string.logout_alert_dialog_message))
+
+        builder.setPositiveButton(getString(R.string.logout_alert_dialog_pos_btn_text)) { dialog, which ->
+            logout()
+        }
+        builder.setNegativeButton(getString(R.string.logout_alert_dialog_neg_btn_text)) { dialog, which ->
+            //Do nothing
+        }
+
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    private fun logout() {
+        intent = Intent(this, LoginActivity::class.java)
+        Firebase.auth.signOut()
+        Toast.makeText(applicationContext, getString(R.string.logout_success_toast_text), Toast.LENGTH_SHORT).show()
+        startActivity(intent)
     }
 }
