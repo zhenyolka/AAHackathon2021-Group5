54d7204183f05d4235add202e5aa00d3ee00873c
AliIbnAbdul <kosh099@ya.ru>
2021-03-13 13:27:08 +0300

    add: add fragment

---
+++ app/src/main/java/com/example/thingder/fragments/add/AddFragment.kt
@@ -0,0 +1,11 @@
+package com.example.thingder.fragments.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.thingder.R
+
+class AddFragment : Fragment() {
+}
