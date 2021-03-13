4a05f6166b96cecc4c012bd55e82b71961b8742d
Evgeniy <zhenyolka@gmail.com>
2021-03-13 22:46:57 +0300

    Fix CreateItemFragment.kt

--- app/src/main/java/com/example/thingder/fragments/createItem/CreateItemViewModel.kt
+++ app/src/main/java/com/example/thingder/fragments/createItem/CreateItemViewModel.kt
@@ -1,23 +1,12 @@
 package com.example.thingder.fragments.createItem
 
-import android.graphics.Bitmap
 import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.thingder.domain.entities.Thing
 import com.example.thingder.domain.usecases.ICreateItemUseCase
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.ByteArrayOutputStream
-import java.util.*
 
 class CreateItemViewModel(private val iCreateItemUseCase: ICreateItemUseCase) : ViewModel() {
     private var _isCreateSuccess = MutableLiveData<Boolean>()
