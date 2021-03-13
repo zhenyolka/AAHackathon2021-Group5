64751861b7185496bfa7c64bf277aa2a9ca18e87
Aleksandr Evdokimov <alexandr8@gmail.com>
2021-03-13 16:59:44 +0300

    Add splashScreen Fix adding item

---
+++ app/src/main/java/com/example/thingder/activities/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.thingder.activities.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.thingder.MainActivity
+import com.example.thingder.activities.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val viewModel by viewModels<SplashViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = if (viewModel.authorized) {
+            Intent(this, MainActivity::class.java)
+        } else { Intent(this, LoginActivity::class.java)
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+}
