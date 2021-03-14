c6a4177b2f084ed48ee5b5b72272442c0268c647
Aleksandr Evdokimov <alexandr8@gmail.com>
2021-03-14 10:39:16 +0300

    Fix warn: add Locale.Root

--- app/src/main/java/com/example/thingder/data/usecases/FirebaseMappersExt.kt
+++ app/src/main/java/com/example/thingder/data/usecases/FirebaseMappersExt.kt
@@ -2,12 +2,13 @@ package com.example.thingder.data.usecases
 
 import com.example.thingder.domain.entities.Thing
 import com.google.firebase.firestore.QueryDocumentSnapshot
+import java.util.*
 
 
 fun QueryDocumentSnapshot.toDomain(): Thing {
     val id = this.id
-    val title = this[FireConstants.KEY_THING_TITLE].toString().capitalize()
-    val description = this[FireConstants.KEY_THING_DESCRIPTION].toString().capitalize()
+    val title = this[FireConstants.KEY_THING_TITLE].toString().capitalize(Locale.ROOT)
+    val description = this[FireConstants.KEY_THING_DESCRIPTION].toString().capitalize(Locale.ROOT)
     val imageUrl = this[FireConstants.KEY_THING_IMAGE_ID].toString()
     return Thing(
         id =id,
